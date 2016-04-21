f045c1845aa25bf8cb8dcedc81f368a49a2f4d6c
Jacob Garrison <jgarrison1204@gmail.com>
2016-04-21 16:02:45 -0700

    Jury Test and Jury Class. Need to understand this better.

--- lib/jury.rb
+++ lib/jury.rb
@@ -3,7 +3,7 @@ class Jury
 
   def initialize
     @members = []
-    @finalists = {finalist1: "name", finalist2: "name"}
+    @finalists_hash = {}
   end
 
   def add_member(name)
@@ -11,10 +11,20 @@ class Jury
     @members << @name
   end
 
-  def cast_votes(finalist)
-    @finalist = finalist
-    @finalists
+  def cast_votes(finalists)
+    finalists.each {|finalist| @finalists_hash[finalist] = 0}
+    @members.each do |member|
+      puts "#{member.name} voted for #{finalists.sample}"
+      @finalists_hash[finalists.sample] += 1
+    end
+    return @finalists_hash
+  end
+
+  def report_votes(final_votes)
+    final_votes.each {|finalist, score| puts "#{finalist}'s Score:#{score}"}
+  end
+
+  def announce_winner(final_votes)
+    winner = final_votes.max_by { |_, vote| vote }.first
   end
 end
-
-Jury.new.cast_votes("jacob")
