a409311c6ed8405ebb51ac1a15bc9e6e8f4df5b7
Jacob Garrison <jgarrison1204@gmail.com>
2016-04-28 14:21:00 -0700

    Game class passes all tests with comments

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,13 @@
-class Game
+require_relative 'Contestant'
+require_relative 'Tribe'
 
+class Game
   attr_reader :tribes
 
   #Splat operator returns an array and allows undefined numbers of data types into array
   def initialize(*tribes)
     @tribes = tribes
+    @merged_tribe_array = []
   end
 
   #Need to push new_tribe into array of objects (borneo which is an instatization of class Game)
@@ -25,7 +28,25 @@ class Game
 
   #Merge takes an argument as the value to the key name: in the initialize method for class Tribe.
   #@members is an empty array as a place holder to satify the structure of Tribe.
+  #Replaced empty array @members with add_merged_contestants_as_values
+  #Clear_tribes deletes existing values in @tribes array of tribe objects
+  #Calls add_tribe with merged_tribe as an input.  This results in new @tribes with all merged members.
   def merge(name_new_tribe)
-    merged_tribe = Tribe.new({name: name_new_tribe, members: @members = []})
+    merged_tribe = Tribe.new({name: name_new_tribe, members: add_merged_contestants_as_values})
+    clear_tribes
+    add_tribe(merged_tribe)
+    return @tribes[0]
+  end
+
+  #Takes array instanziated at intailize and pushes all members of all tribes into the array
+  #Flatten! returns @merged_tribe_array as one dimenesional array of contestants.
+  def add_merged_contestants_as_values
+    @tribes.each {|tribe_member| @merged_tribe_array << tribe_member.members}
+    @merged_tribe_array.flatten!
+  end
+
+  #Returns the first item in the array (merged contestants) calls members methods and uses sample to randomly select the winner
+  def individual_immunity_challenge
+    contestant_immune_from_elimination = @tribes[0].members.sample
   end
 end
