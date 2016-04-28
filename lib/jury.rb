2bc1fddbec8f8ddffad869b537eab045da03ecf6
Jacob Garrison <jgarrison1204@gmail.com>
2016-04-27 18:56:47 -0700

    Updated and simplified Jury

--- lib/jury.rb
+++ lib/jury.rb
@@ -6,25 +6,30 @@ class Jury
     @finalists_hash = {}
   end
 
-  def add_member(name)
-    @name = name
-    @members << @name
+  def add_member(juror)
+    @members << juror
   end
 
-  def cast_votes(finalists)
-    finalists.each {|finalist| @finalists_hash[finalist] = 0}
-    @members.each do |member|
-      puts "#{member.name} voted for #{finalists.sample}"
-      @finalists_hash[finalists.sample] += 1
+  def cast_votes(finalist)
+    finalist.each {|finalist_ind| @finalists_hash[finalist_ind] = 0}
+    @members.each do |juror|
+      puts "#{juror} voted for #{finalist.sample}"
+      @finalists_hash[finalist.sample] += 1
     end
     return @finalists_hash
   end
 
-  def report_votes(final_votes)
-    final_votes.each {|finalist, score| puts "#{finalist}'s Score:#{score}"}
+  def report_votes(votes)
+    votes.each do |finalist, vote_total|
+      puts "#{finalist} had #{vote_total} votes."
+    end
   end
 
-  def announce_winner(final_votes)
-    winner = final_votes.max_by { |_, vote| vote }.first
+  def announce_winner(finalists_hash)
+    finalists_hash.each do |finalist_ind, vote_total|
+      if finalists_hash[finalist_ind] >= 4
+        return finalist_ind
+      end
+    end
   end
 end
