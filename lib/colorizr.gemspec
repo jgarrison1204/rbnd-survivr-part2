5cffe3c5f97547fdd049aefa7720f0af4eb5435a
Jacob Garrison <jgarrison1204@gmail.com>
2016-04-30 09:43:01 -0700

    Added Colorizr gem and gem spec

---
+++ lib/colorizr.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'colorizr'
+  s.version     = '0.0.2'
+  s.date        = '2016-04-06'
+  s.summary     = "Colors Everywhere!"
+  s.description = "A simple gem to colorize terminal output"
+  s.authors     = ["Jacob Garrison"]
+  s.email       = 'jgarrison1204@gmail.com'
+  s.files       = ["lib/colorizr.rb"]
+  s.homepage    =
+    'https://github.com/jgarrison1204'
+  s.license       = 'MIT'
+end
---
+++ lib/colorizr.rb
@@ -0,0 +1,34 @@
+class String
+  #Class variable (a Hash) that contains number reference per color.
+  @@colors = {red: 31, green: 32, yellow: 33, blue: 34,
+  pink: 35, light_blue: 94, white: 97, light_grey: 37, black: 30}
+
+  def self.create_colors
+    #code to generate all color methods goes here
+    @@colors.each do |color, number|
+      self.send(:define_method, color) do
+        puts "\e[#{number}m #{self}\e[0m"
+      end
+    end
+  end
+
+  def self.whatever
+    self
+  end
+
+  def self.colors
+    #Array that hold all the colors.
+    puts @@colors.keys
+  end
+
+  def self.sample_colors
+    @@colors.each do |color, number|
+      #Send takes the name of the color ":red" and calls the cooresponding methods
+      "This is #{color}".send(color)
+    end
+  end
+end
+
+String.create_colors
+String.colors
+puts String.whatever
