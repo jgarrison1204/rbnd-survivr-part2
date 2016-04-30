14b314cf8ac565a82931447ebae5d6d9695c569a
Jacob Garrison <jgarrison1204@gmail.com>
2016-04-29 17:07:53 -0700

    Jury class refactored and notes added.

--- lib/jury.rb
+++ lib/jury.rb
@@ -1,35 +1,44 @@
 class Jury
   attr_accessor :members
 
+  #creates an empty array that will hold members(instances of class Contestant) of the Jury
+  #create an empty hash that will hold finalists and total_votes
   def initialize
     @members = []
-    @finalists_hash = {}
+    @vote_counter_hash = {}
   end
 
+  #pushs contestants who have been elminated from the merged tribe to the @members array
   def add_member(juror)
     @members << juror
+    puts "#{juror} has been eliminated and is now on the Jury."
   end
 
-  def cast_votes(finalist)
-    finalist.each {|finalist_ind| @finalists_hash[finalist_ind] = 0}
+  #Passes in an array of two strings (names of finalists).  Loops the names of finalists as the key of the @vote_counter_hash
+  #and sets the value of vote_counter_hash to 0.  Must be integer because we will increment later.
+  #Loops through members array (filled with instances of class Contestant) and randomly assigns jury member to vote for finalist.
+  #Increments the value of each finalist randomly selectd with finalist_voted_for
+  def cast_votes(finalists)
+    finalists.each {|finalist| @vote_counter_hash[finalist] = 0}
     @members.each do |juror|
-      puts "#{juror} voted for #{finalist.sample}"
-      @finalists_hash[finalist.sample] += 1
+      finalist_voted_for = finalists.sample
+      puts "#{juror} voted for #{finalist_voted_for}"
+      @vote_counter_hash[finalist_voted_for] += 1
     end
-    return @finalists_hash
+    return @vote_counter_hash
   end
 
-  def report_votes(votes)
-    votes.each do |finalist, vote_total|
+  #iterates through @vote_counter_hash and prints number of votes per finalist
+  def report_votes(vote_counter_hash)
+    vote_counter_hash.each do |finalist, vote_total|
       puts "#{finalist} had #{vote_total} votes."
     end
   end
 
-  def announce_winner(finalists_hash)
-    finalists_hash.each do |finalist_ind, vote_total|
-      if finalists_hash[finalist_ind] >= 4
-        return finalist_ind
-      end
-    end
+  #most_votes holds the highest integer in vote_counter_hash.
+  #iterates through vote_counter_hash and returns finalist name of individual who had the most votes.
+  def announce_winner(vote_counter_hash)
+    most_votes = vote_counter_hash.values.max
+    vote_counter_hash.each {|finalist,vote_total| puts finalist.name if vote_total == most_votes}
   end
 end
